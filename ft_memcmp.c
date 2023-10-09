f17dce6c4fd59ad3af6576c41a6129d7daa4d70f
Nina Reher Gonzalez <nireher-@c2r5s1.42madrid.com>
2023-10-09 23:35:48 +0200

    memcmp added

---
+++ ft_memcmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nireher- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/05 21:10:31 by nireher-          #+#    #+#             */
+/*   Updated: 2023/10/09 21:45:49 by nireher-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned char	*str1;
+	unsigned char	*str2;
+
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	if (n == 0)
+		return (0);
+	while (*str1 == *str2 && n > 1)
+	{
+		str1++;
+		str2++;
+		n--;
+	}
+	return (*str1 - *str2);
+}
