7dfb58943a47a8191687de92020ee84749f85848
Nina Reher Gonzalez <nireher-@c2r5s1.42madrid.com>
2023-10-09 23:31:23 +0200

    memcpy added

---
+++ ft_memcpy.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nireher- <nireher-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/05 21:12:08 by nireher-          #+#    #+#             */
+/*   Updated: 2023/10/06 04:18:40 by nireher-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *mdest, const void *msrc, size_t n)
+{
+	size_t	i;
+
+	if (msrc == NULL && mdest == NULL)
+		return (NULL);
+	i = 0;
+	while (n > i)
+	{
+		((char *)mdest)[i] = ((const char *)msrc)[i];
+		i++;
+	}
+	return (mdest);
+}
