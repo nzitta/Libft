0f9c6a2a69e4a1eb01b8abcc61584fee83dcfc48
Nina Reher Gonzalez <nireher-@c3r7s5.42madrid.com>
2024-01-23 14:28:29 +0100

    strrchr updated

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,11 +6,15 @@
 /*   By: nireher <nireher-@student.42madrid.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 16:18:08 by nireher           #+#    #+#             */
-/*   Updated: 2023/11/21 18:18:32 by nireher-         ###   ########.fr       */
+/*   Updated: 2024/01/23 14:28:18 by nireher          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/* 
+		This function returns a pointer to the last occurrence of 'c' on *s 
+ */
 
 char	*ft_strrchr(const char *s, int c)
 {
