9c24308abab6d0fe204929192f14bad3767698cd
Nina Reher Gonzalez <nireher-@c3r7s5.42madrid.com>
2024-01-23 14:32:39 +0100

    tolower updated

--- ft_tolower.c
+++ ft_tolower.c
@@ -6,13 +6,15 @@
 /*   By: nireher <nireher-@student.42madrid.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 06:07:11 by nireher           #+#    #+#             */
-/*   Updated: 2023/11/14 06:10:32 by nireher          ###   ########.fr       */
+/*   Updated: 2024/01/23 14:32:24 by nireher          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/* this functions converts the letter c to lower case, if possible */
+/* 
+	this functions converts the letter c to lower case, if possible 
+*/
 
 int	ft_tolower(int c)
 {
