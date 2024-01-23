9cba58b3732c666d93e9f9c78899824ad8410e24
Nina Reher Gonzalez <nireher-@c3r7s5.42madrid.com>
2024-01-23 13:19:18 +0100

    memchr updated

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,15 +6,15 @@
 /*   By: nireher <nireher-@student.42madrid.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 19:37:07 by nireher           #+#    #+#             */
-/*   Updated: 2023/11/22 20:38:31 by nireher-         ###   ########.fr       */
+/*   Updated: 2024/01/23 13:18:54 by nireher          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /*
- * This function scans n bytes of the mem  pointed to by s for the first c 
- * c and the bytes of the memory pointed to by s are taken as unsigned char
+ 	This function scans n bytes of the mem  pointed to by s for the first c 
+	and the bytes of the memory pointed to by s are taken as unsigned char
  */
 
 void	*ft_memchr(const void *s, int c, size_t n)
