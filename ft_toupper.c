cc5c1962c60e456f1db9426b8761259c56e7876b
Nina Reher Gonzalez <nireher-@c3r7s5.42madrid.com>
2024-01-23 14:33:04 +0100

    toupper updated

--- ft_toupper.c
+++ ft_toupper.c
@@ -6,13 +6,15 @@
 /*   By: nireher <nireher-@student.42madrid.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 05:57:49 by nireher           #+#    #+#             */
-/*   Updated: 2023/11/14 06:09:59 by nireher          ###   ########.fr       */
+/*   Updated: 2024/01/23 14:32:53 by nireher          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/* this functions converts the letter c to upper case, if possible */
+/* 
+	this functions converts the letter c to upper case, if possible 
+*/
 
 int	ft_toupper(int c)
 {
