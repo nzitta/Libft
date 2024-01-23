b2e44f1d2a0d8e30cddce1de218357ba62071431
Nina Reher Gonzalez <nireher-@c3r7s5.42madrid.com>
2024-01-23 14:32:09 +0100

    substr updated

--- ft_substr.c
+++ ft_substr.c
@@ -6,11 +6,15 @@
 /*   By: nireher <nireher-@student.42madrid.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 00:23:43 by nireher           #+#    #+#             */
-/*   Updated: 2023/11/28 04:12:37 by nireher-         ###   ########.fr       */
+/*   Updated: 2024/01/23 14:31:46 by nireher          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+		this function returns a substring using dinamic mem
+ */
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
